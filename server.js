deecaf4b4fcbe46e02588ec40dea050c11461f31
Louis <louyoon89@gmail.com>
2022-12-12 14:11:43 -0800

    helper created, serverjs get

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+// Helper method for generating unique ids
+const uuid = require("./helper/uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,3 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./public")));
 
+//get requests for notes
+app.get("/api/notes", function (err, res) {
+  try {
+    listData = fs.readFileSync("./db/db.json", "utf8");
+    console.log("Success!");
+    listData = JSON.parse(listData);
+  } catch (err) {
+    console.log("Error!");
+    console.log(err);
+  }
+  res.json(listData);
+});
